877875c6b6b841d0032ead15bb751a4829f039e1
thaa2 <sengsetha2@gmail.com>
2026-06-16 12:43:26 +0700

    add some code inside main

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <AuthManager.h>
+#include <Cart.h>
+#include <Customer.h>
+#include <User.h>
+#include <Order.h>
+#include <MenuManager.h>
+#include <FileManager.h>
+
+int main(){
+     FileManager fm;
+     Inventory inv;
+     OrderManager om;
+     CustomerManager cm;
+     AuthManager auth;
+     MenuManager menu;
+}
