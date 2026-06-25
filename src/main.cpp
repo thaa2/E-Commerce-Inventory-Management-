b059720ccdd8175119764c494d6138a91b862c8b
thaa2 <sengsetha2@gmail.com>
2026-06-25 22:14:42 +0700

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 <<<<<<< HEAD
-#include "include/Inventory.h"
-#include "include/Customer.h"
-#include "include/AuthManager.h"
-#include "include/MenuManager.h"
-#include "include/FileManager.h"
+#include "../include/Inventory.h"
+#include "../include/Customer.h"
+#include "../include/AuthManager.h"
+#include "../include/MenuManager.h"
+#include "../include/FileManager.h"
 
 int main() {
     // 1. Make sure all CSV files exist
@@ -31,35 +31,5 @@ int main() {
     menu.loadOrders("data/orders.csv");
 
     menu.showLoginScreen();
-=======
-#include "../include/AuthManager.h"
-#include "../include/Cart.h"
-#include "../include/Customer.h"
-#include "../include/User.h"
-#include "../include/Order.h"
-#include "../include/MenuManager.h"
-#include "../include/FileManager.h"
-#include "../include/Inventory.h"
-#include <iostream>
-
-int main() {
-    Inventory inventory;
-    CustomerBST customers;
-    AuthManager auth;
-
-    // Load database files
-    inventory.loadFromFile("data/products.csv");
-    customers.loadFromFile("data/customers.csv");
-
-    MenuManager menu(inventory, customers, auth);
-    menu.loadOrders("data/orders.csv");
-
-    // Login screen
-    menu.showLoginScreen();
-
-    // Main application menu
->>>>>>> 2b840b5a7254743075b6a98f06fa7198e2ac62f8
-    menu.showMainMenu();
-
-    return 0;
+        return 0;
 }
